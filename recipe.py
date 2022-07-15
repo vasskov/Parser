2df4fb5873f638ac1da8f5282bd0ce306c94594c
vasskov <vasskov505@gmail.com>
2022-07-15 22:35:01 +0300

    На гілці recipe_refac_addFunctional  Зміни, що мають бути збережені в коміті 	змінено:       recipe.py

--- recipe.py
+++ recipe.py
@@ -9,18 +9,17 @@ class Recipe():
         self.version = 0.1
         self.book_config = book_config
         self.source = book_name
-        self.pdf_item_list = pdf_item_list
+        #self.__pdf_item_list = pdf_item_list
         named_tuple = time.localtime()
         self.creation_time = time_string = time.strftime("%d/%m/%Y, %H:%M:%S", named_tuple)
-        self.manege_recipe_text(pdf_item_list)
+        self.manege_recipe_text(pdf_item_list, book_config)
         self.recipe_hash = self.hashing()
-        print(self.note)
+        print(self.__dict__)
         
-    def manege_recipe_text(self, list):
+    def manege_recipe_text(self, list, book_config):
         self.title = self.__set_title(list)
         self.recipe_text = self.__set_recipe_text(list)
-        self.note = []
-        self.__set_note(list)
+        self.note = self.__set_note(list, book_config)
         self.steps = self.__set_steps(list)
         self.ingredients = self.__set_ingredients(list)
         self.tags = self.__set_tags(list)
@@ -37,17 +36,18 @@ class Recipe():
     def __set_recipe_text(self,list):
         return self.__pdfelements__to__text(list[1::])
 
-    def __set_note(self,list):
+    def __set_note(self,list, book_config):
+        return ["note"]
         if not self.book_config.note.font:
             return
-        for element in self.pdf_item_list:
+        for element in self.__pdf_item_list:
             print(element.font)
             print(self.book_config.note.font)
             if element.font in self.book_config.note.font:
                 print(True)
-                self.note.append(element.text())
-                self.pdf_item_list.remove_element(element)
-        return ['note place holder']
+                note.append(element.text())
+                self.__pdf_item_list.remove_element(element)
+        return note
 
     def __set_steps(self, list):
         return ['steps place holder']
@@ -72,8 +72,10 @@ class Recipe():
         return text
 
     def toJSON(self, human_read=True):
-        return json.dumps({"try":"recipe"})
-        #return json.dumps(self, default=lambda o: o.__dict__, indent=4 if human_read else None)
+        #return json.dumps({"try":"recipe"})
+        return json.dumps(self, default=lambda o: o.__dict__, indent=4 if human_read else None)
+        #return json.dumps(self, default=lambda o: o.__dict__)
+        #return json.dumps(self.__str__())
 
 if __name__ == '__main__':
     pass
