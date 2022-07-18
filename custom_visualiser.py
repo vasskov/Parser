3786c5b1beabe077ffd656cdf774d9ad04002fc0
vasskov <vasskov505@gmail.com>
2022-07-18 21:30:46 +0300

    cus_vis refactored

--- custom_visualiser.py
+++ custom_visualiser.py
@@ -5,10 +5,10 @@ from typing import Optional
 import tkinter as tk
 
 #bug import
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from enum import IntEnum
-from typing import Dict, Optional, List
+#from matplotlib.figure import Figure
+#from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+#from enum import IntEnum
+from typing import Dict, Optional #,List
 
 #confirmation
 from py_pdf_parser.visualise.background import get_pdf_background
@@ -18,7 +18,8 @@ from py_pdf_parser.filtering import ElementList
 
 #refactoring
 from custom_visualiser_confirmatiom import Custom_Visualiser_Confirmation
-from custom_visualiser_config_window import Custom_Visualiser_Config_Window
+from custom_visualiser_config_window import Custom_Visualiser_Config_Window, MouseButton
+
 
 STYLES_Confirm = {
     "title": {"color": "#38cc25", "linewidth": 1, "alpha": 0.7},
@@ -28,14 +29,12 @@ STYLES_Confirm = {
     "note": {"color": "#a010de", "linewidth": 1, "alpha": 0.5},
 }
 
-class MouseButton(IntEnum):
-    LEFT = 1
-    MIDDLE = 2
-    RIGHT = 3
-    BACK = 8
-    FORWARD = 9
 
-class Custom_Visualiser(PDFVisualiser, Custom_Visualiser_Confirmation):
+class Custom_Visualiser(
+    PDFVisualiser,
+    Custom_Visualiser_Confirmation,
+    Custom_Visualiser_Config_Window
+    ):
     __clicked_elements: Dict[MouseButton, "PDFElement"] = {}
     __info_text: Optional["Text"] = None
 
@@ -54,7 +53,7 @@ class Custom_Visualiser(PDFVisualiser, Custom_Visualiser_Confirmation):
             self.confirmation_config()
             
         if self.config_window:
-            self.__info_fig, self.__info_text = self.__read_config_window()
+            self.__info_fig, self.__info_text = self.read_config_window()
 
             self.title = []
             self.ignore = []
@@ -63,14 +62,7 @@ class Custom_Visualiser(PDFVisualiser, Custom_Visualiser_Confirmation):
             self.ingredient = []
             self.tags = []
             self.__selected_element = None
-    '''
-    def divide_fonts(self, Result_dict):
-        by_font = {}
-        for key in Result_dict.keys():
-            by_font[key] = Result_dict[key]['font']
-        print(by_font)
-        return by_font
-    '''
+
     def __plot_current_page(self):
         self.__ax.cla()
         # draw PDF image as background
@@ -101,124 +93,6 @@ class Custom_Visualiser(PDFVisualiser, Custom_Visualiser_Confirmation):
     def __element_style(self, _elements, style):
         for element in _elements:
             self._PDFVisualiser__plot_element(element, style)
-
-    def __make_interface(self):
-        title_button = tk.Button(self.read_window, width=15, text="Title", command=self.__write_title_element)
-        ingredients_button = tk.Button(self.read_window, text="Ingredients", command=self.__write_ingredients_element)
-        step_button = tk.Button(self.read_window, text="Steps", command=self.__write_steps_element)
-        note_button = tk.Button(self.read_window, text="Note", command=self.__write_note_element)
-        ignore_button = tk.Button(self.read_window, text="Ignore", command=self.__write_ignore_element)
-        done_button = tk.Button(self.read_window, text="Done", command=self.__done_conf)
-        #tags will be in separete window
-
-        title_button.pack()
-        ingredients_button.pack()
-        step_button.pack()
-        note_button.pack()
-        ignore_button.pack()
-        done_button.pack()
-
-    def __done_conf(self):
-        print('Done')
-        self.Result = {
-            'title': self.title,
-            'ingredients': self.ingredient,
-            'step': self.steps,
-            'note': self.note,
-            'ignored': self.ignore,
-        } 
-        print(self.Result)
-        self.root.destroy()
-        return self.Result
-
-    def __write_title_element(self):
-        if self.__selected_element is not None:
-            self.title.append(self.__selected_element)
-            self.__selected_element = None
-
-    def __write_ingredients_element(self):
-        if self.__selected_element is not None:
-            self.ingredient.append(self.__selected_element)
-            self.__selected_element = None
-
-    def __write_steps_element(self):
-        if self.__selected_element is not None:
-            self.steps.append(self.__selected_element)
-            self.__selected_element = None
-
-    def __write_note_element(self):
-        if self.__selected_element is not None:
-            self.note.append(self.__selected_element)
-            self.__selected_element = None
-
-    def __write_ignore_element(self):
-        if self.__selected_element is not None:
-            self.ignore.append(self.__selected_element)
-            self.__selected_element = None
-
-    def __read_config_window(self):
-        self.read_window = tk.Toplevel(self.root)
-        self.read_window.geometry(f'{int(self.__screen_width*0.98)}x{int(self.__screen_height*0.65)}+{self.__screen_width*2}+0')
-        #read_window.grab_set()
-
-        info_fig = Figure()
-        canvas = FigureCanvasTkAgg(info_fig, self.read_window)
-        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-        self.__make_interface()
-        self.canvas.mpl_connect("button_press_event", self.__on_mouse_click)
-
-        info_text = info_fig.text(
-            0.01,
-            0.5,
-            "",
-            horizontalalignment="left",
-            verticalalignment="center",
-        )
-        return info_fig, info_text
-
-    def __on_mouse_click(self, event: "MouseEvent"):
-        print("click")
-        if event.button == MouseButton.MIDDLE:
-            self.__clear_clicked_elements()
-            return
-        for rect in self.__ax.patches:
-            if not rect.contains(event)[0]:
-                continue
-            # rect is the rectangle we clicked on!
-            self.__clicked_elements[event.button] = rect.element
-            self.__selected_element = rect.element
-            print(rect.element)
-            self.__update_text()
-            return
-
-    def __clear_clicked_elements(self):
-        self.__clicked_elements = {}
-        self.__update_text()
-
-    def _get_element_info(self, element: Optional["PDFElement"]) -> List[str]:
-        if not element:
-            return ["Click an element to see details"]
-        return [
-                f"Text: {element.text(stripped=False)}",
-                f"Font: {element.font}",
-                f"Tags: {element.tags}",
-                f"Bounding box: {element.bounding_box}",
-                f"Width: {element.bounding_box.width}",
-                f"Height: {element.bounding_box.height}",
-            ]    
-
-    def __update_text(self):
-        self.__info_text.set_text(self.__get_clicked_element_info(self.__clicked_elements))
-        self.__info_fig.canvas.draw()
-
-    def __get_clicked_element_info(self, clicked_elements: Dict[MouseButton, "PDFElement"]) -> str:
-        left_element = clicked_elements.get(MouseButton.LEFT)
-        output = []
-        output.append("Clicked element:")
-        output.append("---------------------")
-        output += self._get_element_info(left_element)
-        output.append("")
-        return "\n".join(output)
 
 def custom_visualise(
     document: PDFDocument,
--- custom_visualiser_config_window.py
+++ custom_visualiser_config_window.py
@@ -1,2 +1,135 @@
+import tkinter as tk
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from typing import Dict, Optional, List
+from enum import IntEnum
+
+class MouseButton(IntEnum):
+    LEFT = 1
+    MIDDLE = 2
+    RIGHT = 3
+    BACK = 8
+    FORWARD = 9
+
 class Custom_Visualiser_Config_Window:
-    pass
+    def read_config_window(self):
+        self.__screen_width = int(self.root.winfo_screenwidth() / 2)
+        self.__screen_height = self.root.winfo_screenheight()
+        self.read_window = tk.Toplevel(self.root)
+        self.read_window.geometry(f'{int(self.__screen_width*0.98)}x{int(self.__screen_height*0.65)}+{self.__screen_width*2}+0')
+        #read_window.grab_set()
+
+        info_fig = Figure()
+        canvas = FigureCanvasTkAgg(info_fig, self.read_window)
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        self.__make_interface()
+        self.canvas.mpl_connect("button_press_event", self.__on_mouse_click)
+
+        info_text = info_fig.text(
+            0.01,
+            0.5,
+            "",
+            horizontalalignment="left",
+            verticalalignment="center",
+        )
+        return info_fig, info_text
+
+    def __make_interface(self):
+        title_button = tk.Button(self.read_window, width=15, text="Title", command=self.__write_title_element)
+        ingredients_button = tk.Button(self.read_window, text="Ingredients", command=self.__write_ingredients_element)
+        step_button = tk.Button(self.read_window, text="Steps", command=self.__write_steps_element)
+        note_button = tk.Button(self.read_window, text="Note", command=self.__write_note_element)
+        ignore_button = tk.Button(self.read_window, text="Ignore", command=self.__write_ignore_element)
+        done_button = tk.Button(self.read_window, text="Done", command=self.__done_conf)
+        #tags will be in separete window
+
+        title_button.pack()
+        ingredients_button.pack()
+        step_button.pack()
+        note_button.pack()
+        ignore_button.pack()
+        done_button.pack()
+
+    def __write_title_element(self):
+        if self.__selected_element is not None:
+            self.title.append(self.__selected_element)
+            self.__selected_element = None
+
+    def __write_ingredients_element(self):
+        if self.__selected_element is not None:
+            self.ingredient.append(self.__selected_element)
+            self.__selected_element = None
+
+    def __write_steps_element(self):
+        if self.__selected_element is not None:
+            self.steps.append(self.__selected_element)
+            self.__selected_element = None
+
+    def __write_note_element(self):
+        if self.__selected_element is not None:
+            self.note.append(self.__selected_element)
+            self.__selected_element = None
+
+    def __write_ignore_element(self):
+        if self.__selected_element is not None:
+            self.ignore.append(self.__selected_element)
+            self.__selected_element = None
+
+    def __done_conf(self):
+        print('Done')
+        self.Result = {
+            'title': self.title,
+            'ingredients': self.ingredient,
+            'step': self.steps,
+            'note': self.note,
+            'ignored': self.ignore,
+        } 
+        print(self.Result)
+        self.root.destroy()
+        return self.Result
+
+    def __on_mouse_click(self, event: "MouseEvent"):
+        print("click")
+        if event.button == MouseButton.MIDDLE:
+            self.__clear_clicked_elements()
+            return
+        for rect in self._Custom_Visualiser__ax.patches:
+            if not rect.contains(event)[0]:
+                continue
+            # rect is the rectangle we clicked on!
+            self._Custom_Visualiser__clicked_elements[event.button] = rect.element
+            self.__selected_element = rect.element
+            print(rect.element)
+            self.__update_text()
+            return
+
+    def __clear_clicked_elements(self):
+        self.__clicked_elements = {}
+        self.__update_text()
+
+    def _get_element_info(self, element: Optional["PDFElement"]) -> List[str]:
+        if not element:
+            return ["Click an element to see details"]
+        return [
+                f"Text: {element.text(stripped=False)}",
+                f"Font: {element.font}",
+                f"Tags: {element.tags}",
+                f"Bounding box: {element.bounding_box}",
+                f"Width: {element.bounding_box.width}",
+                f"Height: {element.bounding_box.height}",
+            ]    
+
+    def __update_text(self):
+        self._Custom_Visualiser__info_text.set_text(
+            self.__get_clicked_element_info(self._Custom_Visualiser__clicked_elements)
+            )
+        self._Custom_Visualiser__info_fig.canvas.draw()
+
+    def __get_clicked_element_info(self, clicked_elements: Dict[MouseButton, "PDFElement"]) -> str:
+        left_element = clicked_elements.get(MouseButton.LEFT)
+        output = []
+        output.append("Clicked element:")
+        output.append("---------------------")
+        output += self._get_element_info(left_element)
+        output.append("")
+        return "\n".join(output)
